eff2cbdd05dd1cef01554fc694ab0c989cb47d86
Lucas Dumer <lucasdumer@gmail.com>
2019-11-09 11:12:26 -0300

    Adicionado envs

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
 import os
+import csv
 from flask import Flask
+from flask.ext.mysqldb import MySQL
 
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = os.environ['MYSQL_HOST']
+app.config['MYSQL_USER'] = os.environ['MYSQL_USER']
+app.config['MYSQL_PASSWORD'] = os.environ['MYSQL_PASSWORD']
+app.config['MYSQL_DB'] = os.environ['MYSQL_DB']
+mysql = MySQL(app)
 
 @app.route("/")
 def index():
-    return "<h1>Hello World</hi>"
+    return "<h1>spinne!"
+
+@app.route("/see")
+def index():
+    return os.environ['MYSQL_USER']
+
+# @app.route("/run")
+# def run():
+    # query = "SELECT * from indicadores where indicador1 is not null and indicador2 is not null"
+    # mycursor.execute(query)
+    # row = mycursor.fetchone()
+    # colunas = ('id', 'cd_orgao', 'nr_licitacao', 'ano_licitacao', 'vl_licitacao', 'indicador1', 'indicador2')
+    # with open('tmp/indicadores.csv', 'a') as csvFile:
+    #     writer = csv.writer(csvFile)
+    #     writer.writerow(colunas)
+    #     while row is not None:
+    #         writer.writerow(row)
+    #         row = mycursor.fetchone()
+    # csvFile.close()
+
+# @app.route("/run")
+# def licitacoes(): 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
